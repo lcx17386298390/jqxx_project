ece94b4bc5b9ef467d51d4521e9ee61f359678bb
lcx <2948065094@qq.com>
2024-03-28 11:46:29 +0800

    gui和车牌图像提取

---
+++ project/gui.py
@@ -0,0 +1,327 @@
+import tkinter as tk
+from tkinter import ttk,filedialog
+import logging
+import 图像提取
+from concurrent.futures import ThreadPoolExecutor
+import threading
+import queue
+import os
+import time
+
+threadPool = ThreadPoolExecutor(max_workers=5)
+
+
+class Application(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("车牌识别系统")
+        self.geometry("1200x800")
+        self.resizable(False, False)
+        # 训练状体： 设置：可训练； 未设置：不可训练
+        self.train_status = threading.Event()
+        self.train_status.set()
+ 
+        # 功能子页面列表
+        self.frames = {}
+        for F in (SetPage, TrainPage, TestPage, YuCePage):
+            frame = F(self)
+            frame.grid(row=1, column=0, sticky="nsew")
+            self.frames[F] = frame
+
+        # 导航按钮
+        self.nav_button = NavButton(self)
+        self.nav_button.frame.grid(row=0, column=0, sticky="nw")  # 注意这里的 "nw"
+        self.nav_button.show_frame(TrainPage)
+    
+# 设置页面
+class SetPage(ttk.Frame):
+    def __init__(self, master):
+        super().__init__(master)
+        self.configure(width=1170, height=400)
+        self.grid(sticky='nsew') 
+        master.grid_rowconfigure(1, weight=1)
+        master.grid_columnconfigure(0, weight=1) 
+        self.grid_propagate(1)
+        label = ttk.Label(self, text="设置")
+        label.place(x=180, y=140)        
+
+# 训练页面
+class TrainPage(ttk.Frame):
+    def __init__(self, master):
+        super().__init__(master)
+        self.progress_nums = 0 # 进度条数值
+        self.progress_nums_all = 9999999999999 # 进度条总数值
+        # self.configure(width=1170, height=400)
+        self.grid(sticky='nsew')
+        master.grid_rowconfigure(1, weight=1)
+        master.grid_columnconfigure(0, weight=1) 
+        self.grid_propagate(1) # 使窗口自适应大小
+
+        # 创建显示块
+        self.view_frame = ttk.Frame(self,height=500)
+        self.view_frame.pack(side=tk.TOP,fill=tk.BOTH,expand=True)
+        # label = ttk.Label(self.view_frame, text="数据集训练&测试")
+        # label.place(x=180, y=140)
+
+        # 加入文件夹选择frame(按钮+输入框)
+        self.view_frame.select_frame = ttk.Frame(self.view_frame, width=200, height=100)
+        self.view_frame.select_frame.folder_path = tk.StringVar()
+        # self.view_frame.select_frame.folder_path.set('      请选择训练文件夹')
+        self.view_frame.select_frame.folder_path.set('D:/My_Code_Project/三下机器学习课设/解压数据包/车牌/CCPD2020/test')
+        # 路径显示框
+        self.view_frame.select_frame.folder_entry = ttk.Entry(self.view_frame.select_frame, textvariable=self.view_frame.select_frame.folder_path, state=tk.DISABLED)
+        self.view_frame.select_frame.folder_entry.place(x=0, y=0)
+        # 选择按钮
+        self.view_frame.select_frame.select_button = ttk.Button(self.view_frame.select_frame, text="训练选择", command=self.select_folder)
+        self.view_frame.select_frame.select_button.place(x=0,y=30)
+        # 训练按钮
+        self.view_frame.select_frame.train_button = ttk.Button(self.view_frame.select_frame, text="开始训练", command=self.train_click_add_thread)
+        self.view_frame.select_frame.train_button.place(x=75,y=30)
+        self.view_frame.select_frame.place(x=20,y=10)
+        # 进度条初始化
+        self.progress_bar = self.ProgressBar(self)
+        self.progress_bar.place(x=0, y=100)
+
+        # 创建日志输出台
+        self.log_frame = LogClass(self)
+        self.log_frame.frame.pack(side=tk.BOTTOM, fill=tk.BOTH)
+
+    # 选择按钮点击事件
+    def select_folder(self):
+        old_folder = self.view_frame.select_frame.folder_path.get()
+        folder_selected = filedialog.askdirectory()
+        if not folder_selected:
+            folder_selected = old_folder
+        self.view_frame.select_frame.folder_path.set(folder_selected)
+
+    # 训练按钮点击事件加入线程
+    def train_click_add_thread(self):
+        threading.Thread(target=self.train_click).start()
+
+    # 训练按钮点击事件
+    def train_click(self):
+        folder_path = self.view_frame.select_frame.folder_path.get()
+        if folder_path == '      请选择训练文件夹' or not folder_path:
+            self.log_frame.add_log("请选择训练文件夹", 'warning')
+        else:
+            # 开始训练
+            if self.master.train_status.is_set():
+                self.view_frame.select_frame.train_button.config(text="停止训练")
+                self.master.train_status.clear()
+                self.log_frame.add_log("开始训练\n训练进度······", 'info')
+                # 禁用选择文件夹按钮
+                self.view_frame.select_frame.select_button.config(state=tk.DISABLED, cursor='no')
+                self.train_data_handle = self.TrainDateHandle( folder_path,self)
+                # 进度条更新
+                self.progress_bar.update_progressbar()
+                # 使用多个线程训练
+                future = threadPool.submit(self.train_data_handle.train_start())
+                
+            else:     # 停止训练
+                self.view_frame.select_frame.train_button.config(text="开始训练")
+                self.log_frame.add_log("人为停止训练", 'info')
+                self.master.train_status.set()
+                print("人为停止训练")
+                # 启用选择文件夹按钮
+                self.view_frame.select_frame.select_button.config(state=tk.NORMAL, cursor='arrow')
+
+    # 弃用
+    # 等待任务完成并记录完成情况
+    def wait_for_task_and_log_completion(self,future):
+        result = future.result()  # 阻塞直到任务完成
+        self.log_frame.add_log("训练完成", 'info')
+
+    # 训练数据处理类
+    class TrainDateHandle:
+        def __init__(self, folder_path, master):
+            self.master = master
+            self.folder_path = folder_path
+            self.image_info_dict = {}
+        # 训练启动方法
+        def train_start(self):
+            self.get_image_info()
+        
+        # 获取图像信息
+        def get_image_info(self):
+            files = os.listdir(self.folder_path)
+            self.txtq = 图像提取.TuXiangTiQu(self.folder_path)
+            for name in files:
+                # 判断是否暂停
+                if self.master.master.train_status.is_set():
+                    self.master.log_frame.add_log("人为停止训练", 'info')
+                    self.master.progress_nums = 0
+                    print(self.txtq.image_info_dict)
+                    return
+                # 获取图片信息
+                image_info = self.txtq.start(name)
+                self.master.progress_nums += 1
+                self.master.log_frame.add_log("图片信息：{}".format(image_info),'info')
+                print("图片信息：{}".format(image_info))
+                self.master.log_frame.add_log("训练进度：{}/{}".format(self.master.progress_nums, len(files)), 'info')
+                self.master.progress_nums_all = len(files) # 进度条总数值 
+
+    # 弃用
+    # 训练事件类(初始化便new一个类，跟按钮单独开，按钮改变状态，防止线程锁死)
+    class TrainEvent:
+        def __init__(self,master):
+            self.master = master
+        def set_status(self, master):
+            self.master.view_frame.select_frame.train_button.config(text="停止训练")
+            self.master.master.train_status.set()
+            self.master.log_frame.add_log("开始训练\n训练进度······", 'info')
+            # 禁用选择文件夹按钮
+            self.master.view_frame.select_frame.select_button.config(state=tk.DISABLED, cursor='no')
+            self.master.train_data_handle = self.TrainDateHandle(self.master.folder_path,self)
+            # 使用多个线程训练
+            future = threadPool.submit(self.train_data_handle.train_start)
+            # threadPool.shutdown(wait=False)
+            # 等到线程结 束 后，再执行后续操作
+            threading.Thread(target=self.wait_for_task_and_log_completion, args=(future,)).start()
+
+    # 弃用
+    # 训练状态类（因使用线程锁单独取类）
+    class TrainStatus:
+        def __init__(self):
+            self.status = True  # 训练状态(True:未训练，False:训练中)
+            self.status_lock = threading.Lock()
+        def get_status(self):
+            self.status_lock.acquire()
+            status = self.status
+            self.status_lock.release()
+            return status
+            # with self.status_lock:
+            #     return self.status
+        def set_status(self, status):
+            with self.status_lock:
+                self.status = status
+
+    # 进度条类
+    class ProgressBar(ttk.Frame):
+        def __init__(self,master):
+            super().__init__(master, width=300, height=100)
+            self.master_trainPage = master
+            self.label = ttk.Label(self, text="训练进度：")
+            self.label.place(x=0, y=0)
+            self.progressbar = ttk.Progressbar(self, length=200, mode='determinate') # 显示进度条进度
+            self.progressbar.place(x=60, y=0)
+            self.progressbar_label = ttk.Label(self, text="0%", width=3 , font=("微软雅黑", 8))
+            self.progressbar_label.place(x=self.progressbar.winfo_reqwidth()//2+60, y=11,anchor='center', height=16)
+        
+        # 进度条更新（参数：进度数量）
+        def update_progressbar(self):
+            # 判断是否暂停，改标签
+            if self.master_trainPage.master.train_status.is_set():
+                # self.progressbar['value'] = 0
+                self.progressbar_label['text'] = "训练中断..."
+                # self.progressbar_label = ttk.Label(self, text="0%", width=3 , font=("微软雅黑", 8))
+                self.progressbar_label['width'] = 9
+                return
+            self.progressbar_label['width'] = 3
+            self.progressbar['maximum'] = self.master_trainPage.progress_nums_all
+            self.progressbar['value'] = self.master_trainPage.progress_nums
+            self.master_trainPage.after(100, self.update_progressbar) # 递归调用，每100毫秒调用一次update_progressbar()方法
+            self.progressbar_label['text'] = str(int(self.master_trainPage.progress_nums/self.master_trainPage.progress_nums_all*100)) + '%'
+
+
+# 测试页面
+class TestPage(ttk.Frame):
+    def __init__(self, master):
+        super().__init__(master)
+        self.grid(sticky='nsew')
+        master.grid_rowconfigure(1, weight=1)
+        master.grid_columnconfigure(0, weight=1) 
+        self.grid_propagate(1)
+        label = ttk.Label(self, text="训练结果展示")
+        label.place(x=180, y=140)
+
+class YuCePage(ttk.Frame):   
+    def __init__(self, master):
+        super().__init__(master)
+        self.grid(sticky='nsew')
+        master.grid_rowconfigure(1, weight=1)
+        master.grid_columnconfigure(0, weight=1) 
+        self.grid_propagate(1)
+        label = ttk.Label(self, text="预测")
+        label.place(x=180, y=140)
+
+# 自定义的日志处理器，用于将日志输出到指定的文本框中
+class TextHandler(logging.Handler):
+    def __init__(self, text):
+        logging.Handler.__init__(self)
+        self.text = text
+
+    def emit(self, record):
+        msg = self.format(record)
+        self.text.configure(state='normal')
+        self.text.insert(tk.END, msg + '\n')
+        self.text.configure(state='disabled')
+        # 自动滚动到底部
+        self.text.yview(tk.END)
+
+# 打印类，一个frame，包含log按钮、log文本框和滚动条
+class LogClass:
+    def __init__(self, master):
+        self.master = master
+        # 创建日志输出台和滚动条
+        self.frame = ttk.Frame(self.master,height=200)
+        self.scrollbar = tk.Scrollbar(self.frame, orient=tk.VERTICAL)
+        self.console = tk.Text(self.frame, yscrollcommand=self.scrollbar.set)
+        self.scrollbar.config(command=self.console.yview)
+        self.scrollbar.pack(side=tk.RIGHT, fill=tk.Y)  
+        self.console.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+
+        # 创建日志处理器
+        self.logger = logging.getLogger()
+        self.logger.addHandler(TextHandler(self.console))
+        self.logger.setLevel(logging.INFO)
+        logging.warning("日志记录器已启动")
+
+    def add_log(self, log, type='info'):
+        if type == 'info':
+            logging.info(log)
+        elif type == 'warning':
+            logging.warning(log)
+        elif type == 'error':
+            logging.error(log)
+
+# 导航按钮类
+class NavButton:
+    def __init__(self,master):
+        self.master = master
+        # 创建按钮框架
+        style = ttk.Style(self.master)
+        style.configure("TButton", font=("微软雅黑", 8), borderwidth=0)
+
+        self.frame = ttk.Frame(self.master)
+
+        train_button = ttk.Button(self.frame, text="数据集训练", command=lambda: self.show_frame(TrainPage))
+        train_button.pack(side='left', padx=0)
+
+        set_button = ttk.Button(self.frame, text="测试结果", command=lambda: self.show_frame(TestPage))
+        set_button.pack(side='left', padx=0)
+
+        test_button = ttk.Button(self.frame, text="预测", command=lambda: self.show_frame(YuCePage))
+        test_button.pack(side='left', padx=0)
+
+        set_button = ttk.Button(self.frame, text="设置", command=lambda: self.show_frame(SetPage))
+        set_button.pack(side='left', padx=0)
+
+
+    # 显示指定页面
+    def show_frame(self, page):
+        self.master.frame = self.master.frames[page]
+        self.master.frame.tkraise() #tkraise()方法用于将指定的窗口部件移动到其父级的堆栈顶部
+        # if page.__name__ == 'TrainPage':
+        #     # 创建日志输出台
+        #     self.master.log_frame = LogClass(self.master)
+        #     self.master.log _frame.frame.grid(row=2, column=0, sticky="ew")
+        # else :
+        #     if hasattr(self.master, 'log_frame'):
+        #         self.master.log_frame.frame.grid_forget()
+
+
+
+if __name__ == "__main__":
+    app = Application()
+    app.mainloop()
+ 
---
+++ project/图像提取.py
@@ -0,0 +1,136 @@
+import os
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+
+class TuXiangTiQu:
+    def __init__(self, folder_path):
+        self.folder_path = folder_path      # folder_path = 'D:/My_Code_Project/三下机器学习课设/解压数据包/车牌/CCPD2020/test'
+        # 图片信息字典(在此处我设置字典，给类实例化后数据都会存储在这里，所以不必留接口返回信息，看类的字典即可)
+        self.image_info_dict = {}   # 结构：{图片名称：{图片绝对路径：file_path,面积比：mianjibi, 倾斜度：qinxiedu, 边界坐标：bianjie_zuobiao, 顶点坐标：dingdian_list, 车牌号：car_number}}
+        
+
+    # 获取图像信息
+    def get_image_info(self, image_name):
+        # 图像名称中的字段
+        mianjibi = None
+        qinxiedu = None
+        bianjie_zuobiao = None
+        dingdian_list = None
+        car_number = None
+
+        # 省份、字母和广告码
+        provinces = ["皖", "沪", "津", "渝", "冀", "晋", "蒙", "辽", "吉", "黑", "苏", "浙", "京", "闽", "赣", "鲁", "豫", "鄂", "湘", "粤", "桂", "琼", "川", "贵", "云", "藏", "陕", "甘", "青", "宁", "新", "警", "学", "O"]
+        alphabets = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'O']
+        ads = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'O']
+
+        # 使用 '-' 分割图像名称(面积比、倾斜度 2边界框坐标、四个顶点位置、车牌号)
+        splits = image_name.split('-')
+
+        # 提取面积比(车牌面积与整个画面面积的面积比。)
+        mianjibi = float(splits[0][0]+'.'+splits[0][1:])
+        if splits[0][0] != '0':
+            mianjibi = float(splits[0][1]+'.'+splits[0][2:])
+
+        # 提取倾斜度(水平倾斜度和垂直倾斜度。)
+        qinxiedu = list(map(float, splits[1].split('_')))   # [水平倾斜度, 垂直倾斜度]
+
+        # 提取边界框坐标(左上顶点和右下顶点的坐标。)
+        bianjie_zuobiao = [[splits[2].split('_')[0].split('&')[0],splits[2].split('_')[0].split('&')[1]], [splits[2].split('_')[1].split('&')[0],splits[2].split('_')[1].split('&')[1]]] 
+        bianjie_zuobiao = [list(map(int, sublist)) for sublist in bianjie_zuobiao] # [[左上顶点x, 左上顶点y], [右下顶点x, 右下顶点y]]
+
+        # 提取四个顶点位置(整个图像中 LP 的四个顶点的精确 （x， y） 坐标。这些坐标从右下角的顶点开始(顺时针))
+        dingdian_list = [splits[3].split('_')[0].split('&'), splits[3].split('_')[1].split('&'), splits[3].split('_')[2].split('&'), splits[3].split('_')[3].split('&')]
+        dingdian_list = [list(map(int, sublist)) for sublist in dingdian_list] # [[右下角x, 右下角y], [左下角x, 左下角y], [左上角x, 左上角y], [右上角x, 右上角y]]
+        # 提取车牌号
+        car_number = list(map(int, splits[4].split('_')))
+        car_number = provinces[car_number[0]] + \
+                            alphabets[car_number[1]] + \
+                            ''.join([ads[i] for i in car_number[2:]])
+        self.image_info_dict[image_name] = {'file_path': str(self.folder_path+'/'+image_name),'mianjibi': mianjibi, 'qinxiedu': qinxiedu, 'bianjie_zuobiao': bianjie_zuobiao, 'dingdian_list': dingdian_list, 'car_number': car_number}
+        return mianjibi, qinxiedu, bianjie_zuobiao, dingdian_list, car_number # 面积比、倾斜度、边界坐标、顶点坐标、车牌号
+
+    # 提取车牌图片
+    def get_car_number_image(self, image_name):
+        # 车牌的绝对路径
+        image_file = os.path.join(self.folder_path, image_name)
+
+        # 获得车牌的各种信息
+        mianjibi = self.image_info_dict[image_name]['mianjibi']
+        qinxiedu = self.image_info_dict[image_name]['qinxiedu']
+        bianjie_zuobiao = self.image_info_dict[image_name]['bianjie_zuobiao']
+        dingdian_list = self.image_info_dict[image_name]['dingdian_list']
+        car_number = self.image_info_dict[image_name]['car_number']
+        
+        # 提取车牌图片
+        image = cv2.imread(image_file)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        # 定义提取框的左上角和右下角坐标
+        bbox_top_left = (bianjie_zuobiao[0][0], bianjie_zuobiao[0][1])
+        bbox_bottom_right = (bianjie_zuobiao[1][0], bianjie_zuobiao[1][1])
+
+        # 定义车牌的四个顶点的坐标
+        plate_points = np.array(dingdian_list, np.int32)
+
+        # 在原图上画出提取框
+        image_with_bbox = cv2.rectangle(image.copy(), bbox_top_left, bbox_bottom_right, (0, 255, 0), 2)
+
+        # 在原图上画出车牌的四边形
+        image_with_plate = cv2.polylines(image.copy(), [plate_points], isClosed=True, color=(255, 0, 0), thickness=2)
+
+        # 计算车牌的宽和高
+        plate_width = np.sqrt((dingdian_list[0][0] - dingdian_list[1][0])**2 + (dingdian_list[0][1] - dingdian_list[1][1])**2)
+        plate_height = np.sqrt((dingdian_list[1][0] - dingdian_list[2][0])**2 + (dingdian_list[1][1] - dingdian_list[2][1])**2)
+        # 定义转换车牌的四个顶点的坐标
+        dst_points = np.float32([[plate_width, plate_height], [0, plate_height], [0, 0], [plate_width, 0]])
+        # 提取车牌
+        # 计算透视变换矩阵
+        M = cv2.getPerspectiveTransform(np.float32(dingdian_list), dst_points)
+        # 对图像进行透视变换，得到拉正后的车牌图像
+        plate_image = cv2.warpPerspective(image, M, (int(plate_width), int(plate_height)))
+
+        # 保存车牌图像
+        cv2.imwrite('plate_image.jpg', cv2.cvtColor(plate_image, cv2.COLOR_RGB2BGR))
+        
+        # 测试显示
+        # 创建一个新的figure
+        plt.figure()
+
+        # 创建第一个subplot并显示原图
+        plt.subplot(1, 4, 1)
+        plt.imshow(image)
+        plt.title('Original Image')
+
+        # 创建第二个subplot并显示带有提取框的图
+        plt.subplot(1, 4, 2)
+        plt.imshow(image_with_bbox)
+        plt.title('Image with Bounding Box')
+
+        # 创建第三个subplot并显示带有车牌的图
+        plt.subplot(1, 4, 3)
+        plt.imshow(image_with_plate)
+        plt.title('Image with Plate')
+
+        # 车牌
+        plt.subplot(1, 4, 4)
+        plt.imshow(plate_image)
+        plt.title('Plate Image')
+
+        # 显示所有的subplot
+        plt.show()
+
+        # 返回车牌图像
+        return plate_image
+    
+        
+
+    def start(self,image_name):
+        # 获取图像信息
+        self.get_image_info(image_name)
+        # 提取车牌图片
+        car_number_image =  self.get_car_number_image(image_name)
+
+txtq = TuXiangTiQu('D:\My_Code_Project\cv2_image_test')
+txtq.start('03-103_253-267&425_483&565-483&565_271&497_267&425_480&483-0_0_3_25_25_33_25_25-110-47.jpg')
+
+
